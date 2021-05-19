72f15d9de7122d074bb16a9093b5d0fc1552e6c5
Moko <moko@Mokos-MacBook-Air.local>
2021-05-19 22:51:41 +0200

    RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CocktailsNoAlcoholComponent } from './cocktails-no-alcohol/cocktails-no-alcohol.component';
@@ -18,7 +19,13 @@ import { CocktailsListNoAlcoholComponent } from './cocktails-list-no-alcohol/coc
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'cocktail_no_alcohol', component: CocktailsNoAlcoholComponent },
+      { path: '', redirectTo: 'cocktail_no_alcohol', pathMatch: 'full' },
+      { path: 'cocktail_alcohol', component: CocktailsAlcoholComponent },
+      { path: '**', redirectTo: 'cocktail_no_alcohol', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
