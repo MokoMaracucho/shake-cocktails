b776522d9ca5fd66113ef9f7294d1d4d96b1c4b9
Moko <moko@Mokos-MacBook-Air.local>
2021-05-19 21:29:58 +0200

    ng g c cocktails-list-no-alcohol + Alcohol-free cocktail list

--- src/app/cocktails-list-no-alcohol/cocktails-list-no-alcohol.component.ts
+++ src/app/cocktails-list-no-alcohol/cocktails-list-no-alcohol.component.ts
@@ -7,6 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CocktailsListNoAlcoholComponent implements OnInit {
 
+  public title = 'Alcohol-free cocktail list';
+
+  public cocktails_alcohol_free: any[] = [
+    {
+      id: 1,
+      name: 'Bora Bora',
+      description: 'Deze kant -en klare, alcoholvrije cocktail is…',
+      imgUrl: 'assets/img/Shake-cocktail-Bora-Bora-0-300x599.png'
+    },
+    {
+      id: 2,
+      name: 'Bora Bora',
+      description: 'Deze kant -en klare, alcoholvrije cocktail is…',
+      imgUrl: 'assets/img/Shake-cocktail-Bora-Bora-0-300x599.png'
+    },
+    {
+      id: 3,
+      name: 'Bora Bora',
+      description: 'Deze kant -en klare, alcoholvrije cocktail is…',
+      imgUrl: 'assets/img/Shake-cocktail-Bora-Bora-0-300x599.png'
+    },
+    {
+      id: 4,
+      name: 'Bora Bora',
+      description: 'Deze kant -en klare, alcoholvrije cocktail is…',
+      imgUrl: 'assets/img/Shake-cocktail-Bora-Bora-0-300x599.png'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
